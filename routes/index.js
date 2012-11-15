b87e6cf9a8111b06b1aa556f0769dcb84567cb64
Jonathan Rehm <jkrehm@gmail.com>
2012-11-15 08:21:18 -0700

    - Fix for getting the year (instead of getting it from the query results, get it from the current date). - Show exchanges for the current year.

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,10 @@
 
 exports.index = function(req, res){
   var sqlite3 = require('sqlite3').verbose()
-    , db = new sqlite3.Database('exchange.sqlite');
+    , db = new sqlite3.Database('exchange.sqlite')
+    , year = new Date().getFullYear();
 
-  db.all('SELECT * FROM person a LEFT JOIN gift_exchange_info b ON b.giver = a.id ORDER BY random()', function(err, row) {
-    var year = row[0].year;
-
+  db.all('SELECT * FROM person a LEFT JOIN gift_exchange_info b ON year = ? AND b.giver = a.id ORDER BY random()', year, function(err, row) {
     res.render('index', {
       family: row,
       year: year
