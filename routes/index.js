63727d61786a08f3bf1c168e66b007a826508ef3
Jonathan Rehm <jkrehm@gmail.com>
2012-11-15 08:04:08 -0700

    - Won't show who has picked whom (don't want to ruin the surprise!) - Added the year to the title

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,11 @@ exports.index = function(req, res){
     , db = new sqlite3.Database('exchange.sqlite');
 
   db.all('SELECT * FROM person a LEFT JOIN gift_exchange_info b ON b.giver = a.id ORDER BY random()', function(err, row) {
+    var year = row[0].year;
+
     res.render('index', {
-      family: row
+      family: row,
+      year: year
     });
   });
 };
